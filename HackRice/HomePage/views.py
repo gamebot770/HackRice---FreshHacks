70a2272a5f0c37c2c6411beb41a9bf0ef228c05c
gamebot770 <31254203+gamebot770@users.noreply.github.com>
2018-09-15 01:27:56 -0500

    Initial Setup

---
+++ HackRice/HomePage/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,get_object_or_404
+from django.views.generic import ListView
+from django.http import HttpResponse
+from django.shortcuts import redirect
+import datetime
+# Create your views here.
+
+def HomePage(request):
+    return HttpResponse("<h1>ji</h1>")
+
+def aboutPage(request):
+    a=2
+    b=4
+    sum = a + b
+    return render(request,"HomePage/example.html",{"sum":sum})
