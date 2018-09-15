b9ea5211521367b6cd95cfa27b73987ec7559c8b
Daniel Rothfusz <dpr7@rice.edu>
2018-09-15 11:53:30 -0500

    Include Text function in models
    Uses Email to send an sms given valid number and carrier

--- HackRice/HomePage/models.py
+++ HackRice/HomePage/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import requests
 import json
+
 # Create your models here.
 
 class Customer():
@@ -21,7 +22,32 @@ def getCustomers():
     customerDictionary = customers.json()
     return customerDictionary
 
+
 def getPurchases(id):
     url = "http://api.reimaginebanking.com/accounts/{}/purchases?key=a88246e847558936dbafc257d51fea7b".format(id)
     payments = requests.get(url)
     return payments.json()
+
+
+def send_text(number, carrier, url):
+    carrier_list= {"AT&T":"txt.att.net","T-Mobile":"tmomail.net","Verizon":"vtext.com", "Sprint":"messaging.sprintpcs.com",
+                "Virgin Mobile":"vmobl.com","Tracfone":"mmst5.tracfone.com","Metro PCS":"mymetropcs.com",
+                "Boost Mobile":"sms.myboostmobile.com","Cricket":"sms.cricketwireless.net","Republic Wireless":"text.republicwireless.com",
+                "Google Fi":"msg.fi.google.com","U.S. Cellular":"email.uscc.net","Ting":"message.ting.com",
+                "Consumer Cellular":"mailmymobile.net","C-Spire":"cspire1.com","PagePlus":"ytext.com"}
+
+    message = MIMEMultipart()
+    message['From'] = "expenditureender@gmail.com"
+    message['To'] = str(number) + "@" + carrier_list[carrier]
+
+    text = ("From: %s\r\nTo: %s\r\nSubject: \r\n\r\n"
+            % (message['From'], ", ".join(message['To'])))
+    text += "Mark your purchase here: " + url + "\r\n"
+
+    message.attach(MIMEText(text.encode("utf-8"), "plain", "utf-8"))
+
+    server = smtplib.SMTP("smtp.gmail.com")
+    server.starttls()
+    server.login(message["From"], "kvgiaowqxzwbhfku")
+
+    server.sendmail(message["From"], [message["To"]], text)
