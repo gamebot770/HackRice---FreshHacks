eae3a392aef679c6d7163f88456e22228e90f1b6
gamebot770 <31254203+gamebot770@users.noreply.github.com>
2018-09-15 04:19:24 -0500

    Added GetCustomer and GetPayments

--- HackRice/HomePage/models.py
+++ HackRice/HomePage/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+import requests
+import json
 # Create your models here.
+
+class Customer():
+    def __init__(self):
+        
+        id= "",
+        type= "",
+        nickname= "",
+        rewards= 0,
+        balance= 0,
+        account_number= "",
+        customer_id= ""
+
+
+def getCustomers():
+    url = "http://api.reimaginebanking.com/accounts?key=a88246e847558936dbafc257d51fea7b"
+    customers = requests.get(url)
+    customerDictionary = customers.json()
+    return customerDictionary
+
+def getPurchases(id):
+    url = "http://api.reimaginebanking.com/accounts/{}/purchases?key=a88246e847558936dbafc257d51fea7b".format(id)
+    payments = requests.get(url)
+    return payments.json()
--- HackRice/HomePage/views.py
+++ HackRice/HomePage/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,get_object_or_404
 from django.views.generic import ListView
 from django.http import HttpResponse
+import requests
+import json
 from django.shortcuts import redirect
 import datetime
+from .models import *
 # Create your views here.
 
 def HomePage(request):
@@ -12,4 +15,8 @@ def aboutPage(request):
     a=2
     b=4
     sum = a + b
+
+    customers = getCustomers()
+    for customer in customers:
+        print(customer["_id"],getPurchases(str(customer["_id"])))
     return render(request,"HomePage/example.html",{"sum":sum})
