d3e36843f52051709e67fafa340be8e5390ecefc
Daniel Rothfusz <dpr7@rice.edu>
2018-09-16 00:43:23 -0500

    Account Information View
    Created Account information view that passes all account parameters to account view page

--- HackRice/HomePage/views.py
+++ HackRice/HomePage/views.py
@@ -37,3 +37,17 @@ def signUp(request):
 
 def dashboard(request):
     return render(request,"HomePage/dashboard.html",{})
+
+def viewAccounts(request):
+
+    account = getAccount(request.user.userprofile.id)
+    accId = account.id
+    accType = account.type
+    accNickname = account.nickname
+    accRewards = account.rewards
+    accBalance = account.balance
+    accNumber = account.account_number
+    customerId = account.customer_id
+
+    return render(request,"HomePage/viewAccounts.html",{"accId":accId, "accType":accType, "accNickname":accNickname,
+                    "accRewards":accRewards, "accBalance":accBalance, "accNumber":accNumber, "customerId":customerId})
